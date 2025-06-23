a18edda48ba89df8a7c7ba7c295464fc5f681b9d
jeid12 <niyokwizerajd123@gmail.com>
2025-06-23 18:28:34 +0200

    school entity

---
+++ src/entity/School.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+import { Entity, PrimaryGeneratedColumn, Column, Index, OneToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { User } from './User';
+
+@Index('IDX_SCHOOL_NAME', ['name'])
+@Index('IDX_SCHOOL_PROVINCE', ['province'])
+@Index('IDX_SCHOOL_DISTRICT', ['district'])
+@Index('IDX_SCHOOL_SECTOR', ['sector'])
+@Entity()
+export class School {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    province!: string;
+
+    @Column()
+    district!: string;
+
+    @Column()
+    sector!: string;
+
+    @OneToOne(() => User)
+    @JoinColumn({ name: 'user_id' })
+    user!: User;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+}
